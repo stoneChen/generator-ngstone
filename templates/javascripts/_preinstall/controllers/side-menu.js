8c5e836bcd7c04e6770a9a1501659407d19197b6
stoneChen <baby31529@aliyun.com>
2015-01-18 22:32:10 +0800

    添加注释

--- templates/javascripts/_preinstall/controllers/side-menu.js
+++ templates/javascripts/_preinstall/controllers/side-menu.js
@@ -10,6 +10,7 @@
 angular.module('<%= scriptAppName %>')
     .controller('SideMenuCtrl', function ($scope, $location, $window, localStorageService, resourcePool,globalDataService) {
         $scope.menus = globalDataService.get('menu');
+        //从本地存储读取菜单折叠状态
         var store_menuOpened = localStorageService.get('menuItemOpened');
         $scope.menuOpened = store_menuOpened || new Array($scope.menus.length);
         //ajax获取菜单方式
@@ -32,6 +33,7 @@ angular.module('<%= scriptAppName %>')
                 $scope.menuOpened[idx] = (index === idx ? !$scope.menuOpened[idx] : false);
             })
         }
+        //关闭页面时，记录菜单折叠状态
         angular.element($window).on("beforeunload", function () {
             localStorageService.set('menuItemOpened', $scope.menuOpened);
         });
